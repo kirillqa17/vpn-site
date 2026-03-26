9608f0bdb1d4dc1f5fffdf9eb0f3551792cb91a3
Kirill <dungeonmaster070507@gmail.com>
2026-03-26 21:05:55 +0300

    fix: restore telegram-widget.js from telegram.org (requires OAuth iframe)

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -36,7 +36,7 @@ export default function LoginPage() {
 
     if (containerRef.current) {
       const script = document.createElement('script')
-      script.src = '/tg-scripts/telegram-widget.js?22'
+      script.src = 'https://telegram.org/js/telegram-widget.js?22'
       script.async = true
       script.setAttribute('data-telegram-login', BOT_USERNAME)
       script.setAttribute('data-size', 'large')
